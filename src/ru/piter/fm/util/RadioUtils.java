8067bd6e6e0ab81a0ca8a6d49d01f7430fa12851
Ilya Basin <basinilya@gmail.com>
2014-04-27 11:37:56 +0400

    Extract current track time calculation out of PlayerService

--- src/ru/piter/fm/util/RadioUtils.java
+++ src/ru/piter/fm/util/RadioUtils.java
@@ -200,12 +200,14 @@ public class RadioUtils {
     }
 
 
-    public static String getTrackUrl(String channelId) {
+    public static String getCurrentTrackTime(String channelId) {
         TrackCalendar trackCal = new TrackCalendar();
         Date date = new Date(System.currentTimeMillis() - (TIME_MINUTE * 5));
         trackCal.setTime(date);
-        String currentTrack = trackCal.asURLPart();
-        return getTrackUrlNew(channelId, currentTrack);
+        trackCal.set(Calendar.SECOND, 0);
+        trackCal.set(Calendar.MILLISECOND, 0);
+        String currentTrack = trackCal.asTrackTime();
+        return currentTrack;
     }
 
     public static String getTrackUrlNew(String channelId, String currentTrack) {
