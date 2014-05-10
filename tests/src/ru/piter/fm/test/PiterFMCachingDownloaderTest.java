4ade773c17e031e00e8d78a90d663b34bdd7c727
Ilya Basin <basinilya@gmail.com>
2014-05-10 12:31:45 +0400

    x

--- tests/src/ru/piter/fm/test/PiterFMCachingDownloaderTest.java
+++ tests/src/ru/piter/fm/test/PiterFMCachingDownloaderTest.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 import org.foo.StopButtonInstrumentationTestCase;
 
-import ru.piter.fm.player.Synchronizer;
+import ru.piter.fm.player.PiterFMPlayer;
 import ru.piter.fm.util.TrackCalendar;
 import android.os.Handler;
 import android.os.Looper;
@@ -15,23 +15,26 @@ public class PiterFMCachingDownloaderTest extends InstrumentationTestCase {
 
 	public static final String T = "PiterFMPlayerCacheTest";
 
-	private Synchronizer player;
+	private PiterFMPlayer player;
 	private boolean setupOk;
+	private Handler handler;
 
 	protected void setUp() throws Exception {
 		Log.d(T, "setUp");
 		super.setUp();
 		StopButtonInstrumentationTestCase.showStopButton(getInstrumentation(), getName());
 
-		// create player in UI thread
+        handler = new Handler(Looper.getMainLooper());
+
+        // create player in UI thread
 		final Object lock = new Object();
 		synchronized (lock) {
 			setupOk = false;
-			new Handler(Looper.getMainLooper()).post(new Runnable() {
+			handler.post(new Runnable() {
 				@Override
 				public void run() {
 					Log.d(T, "I'm in MainLooper thread");
-					player = new Synchronizer();
+					player = new PiterFMPlayer();
 					synchronized (lock) {
 						lock.notify();
 						setupOk = true;
@@ -51,10 +54,15 @@ public class PiterFMCachingDownloaderTest extends InstrumentationTestCase {
 	}
 
 	public void testB() throws InterruptedException, IllegalArgumentException, SecurityException, IllegalStateException, IOException {
-		String ch = "7822";
-		TrackCalendar trackTime = new TrackCalendar();
-		trackTime.setTrackTime("2014:05:07:12:39:00");
-		player.open(ch, trackTime.asTrackTime());
+	    handler.post(new Runnable() {
+            @Override
+            public void run() {
+                String ch = "7822";
+                TrackCalendar trackTime = new TrackCalendar();
+                trackTime.setTrackTime("2014:05:07:12:39:00");
+                player.open(ch, trackTime.asTrackTime());
+            }
+        });
 		Thread.sleep(1200000); // should be stopped by the stop button on device screen
 	}
 }
